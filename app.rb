260afa2c00bb50468006731e7ca641aaa4cf0002
Terry G Lorber <tgl@rideside.net>
2015-10-21 22:53:37 -0400

    Only RT if the string has been modified

--- app.rb
+++ app.rb
@@ -2,27 +2,27 @@
 require "twitter"
 
 class NPlus
-  @@noun_list = nil
+  @@nouns = []
 
   def self.seven(a_poem)
     words = a_poem.split
 
-    @@noun_list ||= File.open('nounlist.txt').read
+    if @@nouns.empty?
+      noun_list = File.open('nounlist.txt').read
 
-    nouns = []
-
-    @@noun_list.each_line do |line|
-      nouns << line.chomp
+      noun_list.each_line do |line|
+        @@nouns << line.chomp
+      end
     end
 
-    size_of_nouns = nouns.size
+    size_of_nouns = @@nouns.size
 
     indices = Array.new(words.size, -1)
 
     word_count = 0
     words.each do |w|
       noun_count = 0
-      nouns.each do |noun|  
+      @@nouns.each do |noun|  
         if w.gsub(/[.;:,]/,'') == noun
           indices[word_count] = ((noun_count+7)%size_of_nouns)
           break
@@ -40,7 +40,7 @@ class NPlus
       if index == -1
         a_new_poem << words[count]
       else
-        a_new_poem << nouns[index]
+        a_new_poem << @@nouns[index]
       end
       
       a_new_poem << " "
@@ -59,20 +59,20 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
-client.search("/^\s+\w+$/").take(3).each do |tweet|
+client.search("clock").take(3).each do |tweet|
   puts tweet.id
   puts tweet.full_text
   puts tweet.text
 
-  rt = "MT @#{tweet.user.screen_name}: #{NPlus.seven(tweet.text)}"
+  a_new_tweet = NPlus.seven(tweet.text)
+
+  if a_new_tweet != tweet.text
+    rt = "MT @#{tweet.user.screen_name}: #{a_new_tweet}"
   
-  client.update(rt[0,40])
+    puts rt
+    puts rt[0,140]
+    puts "------------------------------------------------------"
+    client.update(rt[0,140])
+  end
 end
-
-tweet = client.home_timeline[0]
-puts tweet.id
-puts tweet.text
-
-
-
 
